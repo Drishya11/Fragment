a83b42e5ecc28924c223c0ec05f5a6595ab0d1de
mancy1998 <deepmansi2017@gmail.com>
2021-03-21 10:29:31 +0545

    added

---
+++ app/src/main/java/com/example/fragment/SimpleFragment.java
@@ -0,0 +1,51 @@
+package com.example.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+
+
+public class SimpleFragment extends Fragment {
+    private static final int YES = 0;
+    private static final int NO = 1;
+
+    public SimpleFragment() {
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        final View rootView = inflater.inflate(R.layout.fragment_simple, container, false);
+        final RadioGroup group = rootView.findViewById(R.id.radio_group);
+
+        group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                View radioButton = group.findViewById(checkedId);
+                int index = group.indexOfChild(radioButton);
+                TextView textView = rootView.findViewById(R.id.fragment_header);
+                switch (index) {
+                    case YES:
+                        textView.setText(R.string.yes_message);
+                        break;
+                    case NO:
+                        textView.setText(R.string.no_message);
+                        break;
+                    default: break;
+                }
+            }
+        });
+
+        return rootView;
+    }
+
+}
+
